905e2c441b67eb0b04fe9d2df3e5e9baf2f4aaf0
kueitosu401 <tisikizinnrey@gmail.com>
2021-06-11 07:28:42 +0000

    課題topページまで

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+resources :homes
+ # get 'books/index'
+ # get 'books/show'
+ #  get 'books/new'
+ #  get 'books/edit'
+ #  get 'bookers/index'
+ #  get 'bookers/show'
+ #  get 'bookers/new'
+ #  get 'bookers/edit'
+ #  get 'blogs/index'
+ #  get 'blogs/show'
+ #  get 'blogs/new'
+ #  get 'blogs/edit' デフォルトであったルートを念のため残しておく ただしhomsは削除
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :books
+
+end
